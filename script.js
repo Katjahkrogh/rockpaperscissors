29219375bfbd2803585731c946b364ccaea65abc
Katjahkrogh <you@example.com>
2023-08-24 08:58:19 +0200

    noter til spil

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Knapper - eventlistners tilføjes
+// Bruger vælger
+// Math random / Brug array til at opsætte sten, saks og papir, så de på den måde får en automatisk position
+// Hånd animation / Her bruges animationend 
+// Korrekte hænder vises / Computeren vælger sin
+// If sætning der afgør hvem der har vundet / der skal være 6
+// Vis korrekt aflsutningsskærm (hører sammen med punktet ovenover) / her skal der også bruges if sætning
